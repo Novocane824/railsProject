7a7b6949eef1cd257aac36a123e0ce3f9c87fab2
Jean wes philippe <jeanwes.pro@gmail.com>
2014-07-02 12:01:13 +0800

    added coffee

--- spec/features/visit_site_spec.rb
+++ spec/features/visit_site_spec.rb
@@ -5,4 +5,20 @@ feature 'visit the site' do
     visit '/'
     expect(page).to have_content 'Hi, There!'
   end
+  scenario 'Privacy' do
+    visit '/privacy'
+    expect(page).to have_content 'Privacy'
+  end
+  scenario 'Terms' do
+    visit '/terms'
+    expect(page).to have_content 'Terms'
+  end
+  scenario 'SUP!' do
+    visit '/'
+    expect(page).to have_content 'SUP!'
+  end
+  scenario 'Contact' do
+    visit '/contact'
+    expect(page).to have_content 'Contact'
+  end
 end
